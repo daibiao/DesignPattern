1da8e10a342625e68ef1b84b14486a13681cab25
daibiao <dai_biao@163.com>
2017-07-14 16:28:35 +0800

    update

--- src/main/java/com/neusoft/simplefactory/HumanFactory.java
+++ src/main/java/com/neusoft/simplefactory/HumanFactory.java
@@ -1,5 +1,11 @@
 package com.neusoft.simplefactory;
-
+/**
+ * 简单工厂模式（简单静态工厂模式）：
+ * 建立一个工厂类，对实现了同一接口的一些类进行实例的创建。
+ * 简单工厂模式的实质是由一个工厂类根据传入的参数，动态决定应该创建哪一个产品类（这些产品类继承自一个父类或接口）的实例。
+ * @author daibiao
+ *
+ */
 public class HumanFactory {
 	private HumanFactory() {
 
